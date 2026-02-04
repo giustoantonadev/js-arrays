6fd21f67577e1628180e93140d35a8298d2177f9
giustoantonadev <giustoantonadev@gmail.com>
2026-02-04 15:41:36 +0100

    bonus exercise 5

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -57,4 +57,8 @@ console.log(isFabioPresent);
 
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+allTeachers = ['Nathan', 'Ed', 'Fabio','Phil', 'Carlo', 'Lewis', 'Luca']
+const teachersString = allTeachers;
+
+console.log(teachersString);
+
