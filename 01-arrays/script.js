b54895da775eeba79e8c62ea46ced6bbf3ed48d2
giustoantonadev <giustoantonadev@gmail.com>
2026-02-04 13:10:05 +0100

    exercise nr 8

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -33,10 +33,13 @@ const index = teachers.indexOf('Lewis')
 console.log(index);
 
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf('Lewis');
+console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length === 0;
+console.log(isTeachersEmpty);
+
 
 console.log(teachers);
